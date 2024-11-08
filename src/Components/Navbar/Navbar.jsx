48ddf303590c842694510a73e05f026d6edbf53c
Adeleye-Adeola <mightydeveloper500@gmail.com>
2024-11-08 20:31:37 +0100

    Worked harder

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 import "./Navbar.css"
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
+    const body = useRef(null)
+    function hamburger() {
+        setToggle(!toggle)
+    }
+
+    useEffect(() => {
+        if(toggle) { 
+        document.body.style.overflow = 'hidden'
+        document.documentElement.style.overflow = 'hidden'
+        } else {
+        document.body.style.overflowX = 'hidden';
+            document.documentElement.style.overflowX = 'hidden';
+            
+        }
+    }, [toggle])
+    
   return (
     <div className='container' id='navbar'>
         <nav className='nav'>
@@ -11,17 +28,20 @@ const Navbar = () => {
                 <img src="/logo.png" alt="" />
                 <span>CrownDev</span>
             </div>
-            <div className='nav-ul'>
+            <div className={`${toggle ? 'true' : 'false'} nav-ul`} id={`${toggle ? 'true' : 'false'}`}>
                 <ul>
                 <li><Link to = "/">Home</Link></li>
                     <li><Link to = "/about">About</Link></li>
                     <li>Projects</li>     
                     <li><HashLink smooth to = "/#rate-sec"> Testimonial</HashLink>  </li>
                 </ul>
-            </div>
-            <div className='nav-btn'>
+                <div className='nav-btn'>
                 <button>Download CV</button>
             </div>
+            </div>
+            <div className='bars' onClick={hamburger}>
+                <i class="fa-solid fa-bars"></i>    
+                </div>
         </nav>
         <div className='design'>
             <img src="/Design-a.png" alt="" />
